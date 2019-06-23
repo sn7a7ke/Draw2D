05b770ec6ded2c4e1899e4d439246533a593d305
Sn7a7ke <sn7a7ke@protonmail.com>
2019-06-23 22:51:38 +0300

    Unit Tests for Line2D (Ctors, IsLineInKB, Kk, Kb)
    Fixed bug in Ctor_kb

--- Plane2D.Tests/Line2DTest.cs
+++ Plane2D.Tests/Line2DTest.cs
@@ -9,18 +9,86 @@ namespace Plane2D.Tests
     {
         Line2D l1;
         Line2D l2;
+        Line2D l3;
 
         [SetUp]
         public void Init()
         {
             l1 = new Line2D(2, 1, -4);
             l2 = new Line2D(-1, 2, 2);
+            l3 = new Line2D(2, 0, 5);
         }
+
+        [Test]
+        public void Ctor_ABC_True()
+        {
+            Assert.IsTrue(l1.Equals(new Line2D(2, 1, -4)));
+        }
+        [Test]
+        public void Ctor_kb_True()
+        {
+            Line2D lin3 = new Line2D(-2, 4);
+            Assert.IsTrue(l1.Equals(lin3));
+        }
+
+        [Test]
+        public void Ctor_TwoPoint_True()
+        {
+            Assert.IsTrue(l1.Equals(new Line2D(new Point2D(2, 0), new Point2D(0, 4))));
+        }
+
+        [Test]
+        public void Ctor_VectorPoint_True()
+        {
+            Assert.IsTrue(l1.Equals(new Line2D(new Vector2D(2, 1), new Point2D(1, 2))));
+        }
+
+        [Test]
+        public void IsLineInKB_True()
+        {
+            Assert.IsTrue(l1.IsLineInKB);
+        }
+
+        [Test]
+        public void IsLineInKB_False()
+        {
+            Assert.IsFalse(l3.IsLineInKB);
+        }
+
+        [Test]
+        public void Kk_True()
+        {
+            Assert.IsNotNull(l2.Kk);
+            double res = l2.Kk ?? 0;
+            Assert.IsTrue(res.Equal(0.5));
+        }
+
+        [Test]
+        public void Kk_IsNull()
+        {
+            Assert.IsNull(l3.Kk);
+        }
+
+        [Test]
+        public void Kb_True()
+        {
+            Assert.IsNotNull(l2.Kb);
+            double res = l2.Kb ?? 0;
+            Assert.IsTrue(res.Equal(-1));
+        }
+
+        [Test]
+        public void Kb_IsNull()
+        {
+            Assert.IsNull(l3.Kb);
+        }
+
         [Test]
         public void IsPerpendicular()
         {
             Assert.IsTrue(l1.IsPerpendicular(l2));
         }
+
         [Test]
         public void DistanceFromPointToLine()
         {
@@ -32,6 +100,7 @@ namespace Plane2D.Tests
         {
             Assert.IsTrue(l1.GetTangent(new Point2D(2, 0)).Equals(l1));
         }
+
         [Test]
         public void GetTangent_Null()
         {
@@ -43,18 +112,21 @@ namespace Plane2D.Tests
         {
             Assert.IsTrue(l1.PerpendicularFromPoint(new Point2D(6, 2)).Equals(l2));
         }
+
         [Test]
         public void PerpendicularFromPoint2()
         {
             Line2D l1 = new Line2D(1, 0, -2);
             Assert.IsTrue(l1.PerpendicularFromPoint(new Point2D(4, 1)).Equals(new Line2D(0, 1, -1)));
         }
+
         [Test]
         public void PerpendicularFromPoint3()
         {
             Line2D l1 = new Line2D(0, 1, -2);
             Assert.IsTrue(l1.PerpendicularFromPoint(new Point2D(1, 4)).Equals(new Line2D(1, 0, -1)));
         }
+
         [Test]
         public void PerpendicularFromPoint_PointOnLine()
         {
@@ -67,11 +139,13 @@ namespace Plane2D.Tests
         {
             Assert.IsTrue(l1.IntersectPerpendicularFromPointWithLine(new Point2D(6, 2)).Equals(new Point2D(2, 0)));
         }
+
         [Test]
         public void Intersect()
         {
             Assert.IsTrue(l1.Intersect(l2).Equals(new Point2D(2, 0)));
         }
+
         [Test]
         public void Intersect2()
         {
@@ -83,22 +157,11 @@ namespace Plane2D.Tests
         {
             Assert.IsTrue(l1.Equals(new Line2D(4, 2, -8)));
         }
+
         [Test]
         public void EqualsBad()
         {
             Assert.IsFalse(l1.Equals(new Line2D(4, 2, -6)));
         }
-
-        [Test]
-        public void Ctor()
-        {
-            Assert.IsTrue(l1.Equals(new Line2D(new Point2D(2, 0), new Point2D(0, 4))));
-        }
-        [Test]
-        public void Ctor2()
-        {
-            Assert.IsTrue(l1.Equals(new Line2D(new Vector2D(2, 1), new Point2D(1, 2))));
-        }
-
     }
 }
--- Plane2D/Line2D.cs
+++ Plane2D/Line2D.cs
@@ -20,21 +20,26 @@ namespace Plane2D
             this.B = B;
             this.C = C;
         }
-        public Line2D(double k, double b) : this(1, -k, -b)
+
+        public Line2D(double k, double b) : this(-k, 1, -b)
         {
         }
+
         public Line2D(Point2D p1, Point2D p2) : this(p2.Y - p1.Y, p1.X - p2.X, p1.Y * p2.X - p1.X * p2.Y)
         {
         }
-        public Line2D(Vector2D vNormal, Point2D pVia) : this(vNormal.X, vNormal.Y, -vNormal.X * pVia.X - vNormal.Y * pVia.Y)
+
+        public Line2D(Vector2D vNormal, Point2D pointVia) : this(vNormal.X, vNormal.Y, -vNormal.X * pointVia.X - vNormal.Y * pointVia.Y)
         {
         }
 
         public double A { get; private set; }
+
         public double B { get; private set; }
+
         public double C { get; private set; }
 
-        public bool IsLineInKB => !B.IsZero();//B != 0;
+        public bool IsLineInKB => !B.IsZero();
         /// <summary>
         /// y = k * x + b; where k - Kk, b - Kb
         /// </summary>
@@ -48,6 +53,7 @@ namespace Plane2D
                     return null;
             }
         }
+
         /// <summary>
         /// y = k * x + b; where k - Kk, b - Kb
         /// </summary>
@@ -62,22 +68,26 @@ namespace Plane2D
             }
         }
 
-        public bool IsParallel(Line2D l) => (A * l.B - B * l.A).IsZero();//A * l.B - B * l.A == 0;
-        public bool IsParallelAxisX() => A.IsZero();//A == 0;
-        public bool IsParallelAxisY() => B.IsZero();//B == 0;
-        public bool IsPerpendicular(Line2D l) => (A * l.A + B * l.B).IsZero();//A * l.A + B * l.B == 0;
-        public bool IsOnLine(Point2D p) => (A * p.X + B * p.Y + C).IsZero();//A * p.X + B * p.Y + C == 0;
+        public bool IsParallel(Line2D l) => (A * l.B - B * l.A).IsZero();
+
+        public bool IsParallelAxisX() => A.IsZero();
+
+        public bool IsParallelAxisY() => B.IsZero();
+
+        public bool IsPerpendicular(Line2D l) => (A * l.A + B * l.B).IsZero();
+
+        public bool IsOnLine(Point2D p) => (A * p.X + B * p.Y + C).IsZero();
 
         public double DistanceFromPointToLine(Point2D p) => Math.Abs(A * p.X + B * p.Y + C) / Math.Sqrt(A * A + B * B);
-        public Vector2D GetNormal => new Vector2D(A, B);
 
+        public Vector2D GetNormal => new Vector2D(A, B);
 
         #region IFunction2D
         public double MaxX
         {
             get
             {
-                if (B.IsZero()) //(B == 0)
+                if (B.IsZero())
                     return -C / A;
                 else
                     return double.PositiveInfinity;
@@ -88,7 +98,7 @@ namespace Plane2D
         {
             get
             {
-                if (A.IsZero()) //(A == 0)
+                if (A.IsZero())
                     return (double)Kb;
                 else
                     return double.PositiveInfinity;
@@ -99,7 +109,7 @@ namespace Plane2D
         {
             get
             {
-                if (B.IsZero()) //(B == 0)
+                if (B.IsZero())
                     return -C / A;
                 else
                     return double.NegativeInfinity;
@@ -110,7 +120,7 @@ namespace Plane2D
         {
             get
             {
-                if (A.IsZero()) //(A == 0)
+                if (A.IsZero())
                     return (double)Kb;
                 else
                     return double.NegativeInfinity;
@@ -124,7 +134,7 @@ namespace Plane2D
         /// <returns>y</returns>
         public List<double> FuncYFromX(double x)
         {
-            if (B.IsZero()) //(B == 0)
+            if (B.IsZero())
                 return null;
             else
                 return new List<double>() { (-A * x - C) / B };
@@ -132,23 +142,22 @@ namespace Plane2D
 
         public List<double> InverseFuncXFromY(double y)
         {
-            if (A.IsZero()) //(A == 0)
+            if (A.IsZero())
                 return null;
             else
                 return new List<double>() { (-B * y - C) / A };
-
         }
+
         public Line2D GetTangent(Point2D p)
         {
             if (FuncYFromX(p.X).Contains(p.Y, new DoubleComparer()))
                 return this;
             return null;
         }
-
         #endregion
 
-
         public Line2D PerpendicularFromPoint(Point2D p) => new Line2D(-B, A, B * p.X - A * p.Y);
+
         public Point2D IntersectPerpendicularFromPointWithLine(Point2D p)
         {
             double zz = A * A + B * B;
@@ -156,6 +165,7 @@ namespace Plane2D
             double yy = (A * (-B * p.X + A * p.Y) - B * C) / zz;
             return new Point2D(xx, yy);
         }
+
         public Point2D Intersect(Line2D line)
         {
             if (IsParallel(line))
@@ -170,12 +180,14 @@ namespace Plane2D
         public double AngleBetweenLines(Line2D l) => Vector2D.AngleBetweenVectors(new Vector2D(A, B), new Vector2D(l.A, l.B));
 
         public override string ToString() => string.Format($"{A} * x + {B} * y + {C} = 0");
+
         public override bool Equals(object obj)
         {
             if (obj == null || !(obj is Line2D))
                 return false;
             return this.Equals(obj as Line2D);
         }
+
         public bool Equals(Line2D otherLine)
         {
             if (otherLine == null)
@@ -184,9 +196,10 @@ namespace Plane2D
             double det = A * otherLine.B - A * otherLine.C - B * otherLine.A + B * otherLine.C + C * otherLine.A - C * otherLine.B;
             return det.IsZero();
         }
-        public override int GetHashCode() => (A.IsZero()) ? (int)(A / B * 101 + C / B) : (int)(B / A * 101 + C / A); //(A != 0) ? (int)(B / A * 101 + C / A) : (int)(A / B * 101 + C / B);
+        public override int GetHashCode() => (A.IsZero()) ? (int)(A / B * 101 + C / B) : (int)(B / A * 101 + C / A);
 
         public static bool operator ==(Line2D obj1, Line2D obj2) => Equals(obj1, obj2); // obj1.Equals(obj2);
+
         public static bool operator !=(Line2D obj1, Line2D obj2) => !Equals(obj1, obj2);
     }
 }
