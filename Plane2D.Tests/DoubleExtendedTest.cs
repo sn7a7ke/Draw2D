7a45285a718eef2e5648ab83a61814dc12ae0a0c
Sn7a7ke <sn7a7ke@protonmail.com>
2019-06-23 22:01:05 +0300

    Added Unit Tests for DoubleExtended class

---
+++ Plane2D.Tests/DoubleExtendedTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+
+namespace Plane2D.Tests
+{
+    [TestFixture]
+    class DoubleExtendedTest
+    {
+        double d1;
+        double d2;
+        [Test]
+        public void Epsilon()
+        {
+            double newEpsilon = 0.00001;
+            DoubleExtended.Epsilon = newEpsilon;
+            Assert.IsTrue(DoubleExtended.Epsilon == newEpsilon);
+        }
+
+        [Test]
+        public void Equal()
+        {
+            d1 = 2.54;
+            d2 = 2.54000000001;
+            Assert.IsTrue(d1.Equal(d2));
+        }
+
+        [Test]
+        public void IsZero()
+        {
+            d1 = 0.00000000001;
+            Assert.IsTrue(d1.IsZero());
+        }
+
+        [Test]
+        public void More()
+        {
+            d1 = 2.64;
+            d2 = 2.57;
+            Assert.IsTrue(d1.More(d2));
+        }
+
+        [Test]
+        public void MoreOrEqual()
+        {
+            d1 = 2.57;
+            d2 = 2.57000000000001;
+            Assert.IsTrue(d1.MoreOrEqual(d2));
+        }
+
+        [Test]
+        public void Less()
+        {
+            d1 = 2.64;
+            d2 = 2.57;
+            Assert.IsTrue(d2.Less(d1));
+        }
+
+        [Test]
+        public void LessOrEqual()
+        {
+            d1 = 2.57000000000001;
+            d2 = 2.57;
+            Assert.IsTrue(d2.LessOrEqual(d1));
+        }
+    }
+}
--- Plane2D/DoubleExtended.cs
+++ Plane2D/DoubleExtended.cs
@@ -4,13 +4,18 @@ namespace Plane2D
 {
     public static class DoubleExtended
     {
-        //private const double epsilon = 0.0000001;
         public static double Epsilon { get; set; } = 0.0000001;
+
         public static bool Equal(this double d1, double d2) => Math.Abs(d1 - d2) < Epsilon;
+
         public static bool IsZero(this double d1) => Equal(d1, 0.0);
+
         public static bool More(this double d1, double d2) => d1 - d2 >= Epsilon;
+
         public static bool MoreOrEqual(this double d1, double d2) => d1 - d2 > -Epsilon;
+
         public static bool Less(this double d1, double d2) => d2 - d1 >= Epsilon;
+
         public static bool LessOrEqual(this double d1, double d2) => d2 - d1 > -Epsilon;
     }
 }
