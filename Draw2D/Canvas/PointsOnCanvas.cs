8be567d261805e67024b4e85e3e058bccd57d016
Sn7a7ke <sn7a7ke@protonmail.com>
2019-07-20 15:21:40 +0300

    Created folder Canvas
    Extracted Polygons2DOnCanvas and PointsOnCanvas classes from Canvas class

---
+++ Draw2D/Canvas/PointsOnCanvas.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Draw2D.Canvas
+{
+    public class PointsOnCanvas
+    {
+        public Point Temporary { get; set; }
+
+        public Point First => List.Count > 0 ? List[0] : new Point(0, 0);
+
+        public List<Point> List { get; private set; }
+
+        public delegate void RefreshPoints();
+
+        public RefreshPoints Refresh { get; set; } // for correct work must be filling
+
+        public PointsOnCanvas()
+        {
+            List = new List<Point>();
+        }
+
+        public void AddPoint(Point point)
+        {
+            List.Add(point);
+            Temporary = point;
+            if (List.Count > 1)
+                Refresh?.Invoke();
+        }
+
+        public void RemovePoint(Point point)
+        {
+            if (List.Count > 0)
+                List.RemoveAt(List.Count - 1);
+            Refresh?.Invoke();
+        }
+    }
+}
---
+++ Draw2D/Canvas/Polygons2DOnCanvas.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using Plane2D;
+
+namespace Draw2D.Canvas
+{
+    public class Polygons2DOnCanvas
+    {
+        public List<Polygon2D> List { get; private set; }
+
+        public Polygon2D Selected { get; private set; }
+
+        public delegate void RefreshPolygons();
+
+        public RefreshPolygons Refresh { get; set; } // for correct work must be filling
+
+        public Polygons2DOnCanvas()
+        {
+            List = new List<Polygon2D>();
+            Selected = null;
+        }
+
+        public bool Add(Polygon2D polygon2D)
+        {
+            if (polygon2D == null)
+                return false;
+            List.Add(polygon2D);
+            Refresh?.Invoke();
+            return true;
+        }
+
+        public bool Remove(Polygon2D polygon2D)
+        {
+            if (!List.Remove(polygon2D))
+                return false;
+            if (Selected == polygon2D)
+                Selected = null;
+            Refresh?.Invoke();
+            return true;
+        }
+
+        public bool Change(Polygon2D currentPolygon2D, Polygon2D newPolygon2D)
+        {
+            if (currentPolygon2D == newPolygon2D)
+                return false;
+            var polygon2D = Find(currentPolygon2D);
+            if (polygon2D == null)
+                return false;
+            polygon2D = newPolygon2D;
+            Select(polygon2D);
+            Refresh?.Invoke();
+            return true;
+        }
+
+        public bool Select(int number)
+        {
+            int count = List.Count;
+            if (count == 0 || number < 0 || number >= count || Selected == List[number])
+                return false;
+            Selected = List[number];
+            Refresh?.Invoke();
+            return true;
+        }
+
+        public bool Select(Polygon2D newSelectedPolygon2D)
+        {
+            if (newSelectedPolygon2D == null)
+                return false;
+            var polygon2D = Find(newSelectedPolygon2D);
+            if (polygon2D == null || Selected == polygon2D)
+                return false;
+            Selected = polygon2D;
+            Refresh?.Invoke();
+            return true;
+        }
+
+        public bool SelectNext()
+        {
+            if (List.Count <= 1)
+                return false;
+            int idSelected = FindId(Selected);
+            Select(++idSelected);
+            Refresh?.Invoke();
+            return true;
+        }
+
+        public bool SelectPrevious()
+        {
+            if (List.Count <= 1)
+                return false;
+            int idSelected = FindId(Selected);
+            Select(--idSelected);
+            Refresh?.Invoke();
+            return true;
+        }
+
+        private Polygon2D Find(Polygon2D polygon2D)
+        {
+            return List.Find(p => p.Equals(polygon2D));
+        }
+
+        private int FindId(Polygon2D polygon2D)
+        {
+            return List.FindIndex(p => p.Equals(polygon2D));
+        }
+    }
+}
