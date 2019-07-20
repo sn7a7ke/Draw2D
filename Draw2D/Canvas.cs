824950f6ff438784ca71fb1f4abc8992cd427ecf
Sn7a7ke <sn7a7ke@protonmail.com>
2019-07-20 14:51:57 +0300

    Methods of act Polygons2DOnCanvas return boolean
    Added SelectNext, SelectPrevious, Find, FindId methods

--- Draw2D/Canvas.cs
+++ Draw2D/Canvas.cs
@@ -205,27 +205,78 @@ namespace Draw2D
                 Selected = null;
             }
 
-            public void Add(Polygon2D polygon2D)
+            public bool Add(Polygon2D polygon2D)
             {
+                if (polygon2D == null)
+                    return false;
                 List.Add(polygon2D);
                 Refresh?.Invoke();
+                return true;
             }
 
-            public void Remove(Polygon2D polygon2D)
+            public bool Remove(Polygon2D polygon2D)
             {
-                List.Remove(polygon2D);
+                if (!List.Remove(polygon2D))
+                    return false;
                 if (Selected == polygon2D)
                     Selected = null;
                 Refresh?.Invoke();
+                return true;
             }
 
-            public void Change(Polygon2D currentPolygon2D, Polygon2D newPolygon2D)
+            public bool Change(Polygon2D currentPolygon2D, Polygon2D newPolygon2D)
             {
+                if (currentPolygon2D == newPolygon2D)
+                    return false;
                 var polygon2D = Find(currentPolygon2D);
+                if (polygon2D == null)
+                    return false;
                 polygon2D = newPolygon2D;
-                if (Selected == currentPolygon2D)
-                    Selected = newPolygon2D;
+                Select(polygon2D);
                 Refresh?.Invoke();
+                return true;
+            }
+
+            public bool Select(int number)
+            {
+                int count = List.Count;
+                if (count == 0 || number < 0 || number >= count || Selected == List[number])
+                    return false;
+                Selected = List[number];
+                Refresh?.Invoke();
+                return true;
+            }
+
+            public bool Select(Polygon2D newSelectedPolygon2D)
+            {
+                if (newSelectedPolygon2D == null)
+                    return false;
+                var polygon2D = Find(newSelectedPolygon2D);
+                if (polygon2D == null || Selected == polygon2D)
+                    return false;
+                Selected = polygon2D;
+                Refresh?.Invoke();
+                return true;
+            }
+
+            public bool SelectNext()
+            {
+                if (List.Count <= 1)
+                    return false;
+                int idSelected = FindId(Selected);
+                Select(++idSelected);
+                Refresh?.Invoke();
+                return true;
+            }
+
+            public bool SelectPrevious()
+            {
+                if (List.Count <= 1)
+                    return false;
+                int idSelected = FindId(Selected);
+                Select(--idSelected);
+                Refresh?.Invoke();
+                return true;
             }
 
             private Polygon2D Find(Polygon2D polygon2D)
@@ -233,23 +284,9 @@ namespace Draw2D
                 return List.Find(p => p.Equals(polygon2D));
             }
 
-            public void Select(int number)
+            private int FindId(Polygon2D polygon2D)
             {
-                int count = List.Count;
-                if (count > 0 && number >= 0 && number < count)
-                {
-                    if (Selected != List[number])
-                    {
-                        Selected = List[number];
-                        Refresh?.Invoke();
-                    }
-                }
-            }
-
-            public void Select(Polygon2D newSelectedPolygon2D)
-            {
-                Selected = Find(newSelectedPolygon2D);
-                Refresh?.Invoke();
+                return List.FindIndex(p => p.Equals(polygon2D));
             }
         }
 
